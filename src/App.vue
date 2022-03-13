c974ac7c6d0938edb43d28acddaf856171d32b38
Gin Pan <hereticanthemp@gmail.com>
2022-03-13 17:56:58 +0800

    Update basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main-layout></main-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainLayout from './layouts/MainLayout.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainLayout
   }
 }
 </script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-container class="wrapper">
+    <el-container>
+      <el-header class="top">
+        <top-nav></top-nav>
+      </el-header>
+      <el-container>
+        <el-aside width="200px" class="side">
+          <side-nav></side-nav>
+        </el-aside>
+        <el-main>
+            <router-view></router-view>
+        </el-main>
+      </el-container>
+      <el-footer class="foot"></el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import SideNav from "./SideNav.vue";
+import TopNav from "./TopNav.vue";
+export default {
+  components: {
+    SideNav,
+    TopNav,
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.side {
+  background-color: whitesmoke;
+}
+
+.top {
+  background-color: cornflowerblue;
+}
+
+.foot {
+  background-color: grey;
+}
+</style>
